c5c36713828e6230b422d675892740fdca789fe6
alpacarlioglu <alpacarlioglu@gmail.com>
2021-12-29 22:11:44 +0300

    6

--- Recursive.c
+++ Recursive.c
@@ -5,13 +5,17 @@ int Faktoriyel(int sayi)
 {   
     if (sayi == 0) 
         return 1;
-    return sayi * Faktoriel(sayi -1); // 0 Olana kadar surekli kendini cagiriyor
+    return sayi * Faktoriyel(sayi -1); // 0 Olana kadar surekli kendini cagiriyor
 }
 
 int main()
 { 
-    int input;
-    printf("Sayi giriniz\n");
-    scanf("%d", &input);
-    int fakt = Faktoriyel(&input);
+    while (1)
+    {
+        int input;
+        printf("Sayi giriniz\n");
+        scanf("%d", &input);
+        int fakt = Faktoriyel(input);
+        printf("%d\n", fakt);        
+    }
 }
